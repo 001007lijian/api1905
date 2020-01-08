3658be8e09aeac8d5024f73f454c6d6ed648665e
lijian <2351584897@qq.com>
2020-01-08 09:20:57 +0800

    pubkey&&privkey

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('test/alipay','TestController@alipay');
+Route::get('/goods','TestController@goods');
+Route::get('/goods2','TestController@goods2');
+Route::get('/test/grab','TestController@grab');
 
 Route::get('alipay/return','alipay\\PayController@return');//同步
 Route::post('alipay/notify','alipay\\PayController@notify');//异步
